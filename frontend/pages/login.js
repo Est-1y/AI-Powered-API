32d89248e554783ee154b0187feb7e2f6d85add2
Esther Obong'o <133119207+Est-1y@users.noreply.github.com>
2025-03-03 13:40:06 +0300

    Create login.js

---
+++ frontend/pages/login.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await axios.post("/api/auth/login", { email, password });
+    localStorage.setItem("token", res.data.token);
+  };
+
+  return (
+    <div>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
